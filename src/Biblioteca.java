7b13b7ea72f392501bfc5d181827d5a1b6c3f7eb
Vitor <vitordeazambuja@gmail.com>
2026-01-11 19:09:33 -0300

    feat: criada classe de biblioteca

---
+++ src/Biblioteca.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+    private List<Livro> livros = new ArrayList<>();
+    private List<Autor> autores = new ArrayList<>();
+    private List<Emprestimo> emprestimos = new ArrayList<>();
+}
