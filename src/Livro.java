fbce18b879a4403085122d2825f488cf767aa9f6
Vitor <vitordeazambuja@gmail.com>
2026-01-11 18:56:30 -0300

    feat: criada classe de livro

---
+++ src/Livro.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Livro {
+    int id;
+    String titulo;
+    Autor autor;
+    boolean disponivel;
+    LocalDate dataCadastro;
+    LocalDate dataAtualizacao;
+}
