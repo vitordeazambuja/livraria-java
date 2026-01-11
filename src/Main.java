08c1bfa0aaedc7e2a41d6ea3dc2a5d503a4ba575
Vitor <vitordeazambuja@gmail.com>
2026-01-11 20:21:24 -0300

    chore: implementada loop de escolha no menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        Scanner sc = new Scanner(System.in);
+        String opcao;
+
+        do{
+            System.out.println("Sistema de Livraria");
+            System.out.println("Deseja ver a lista de livros disponíveis?");
+            opcao = sc.nextLine().trim().toUpperCase();
+
+        } while(opcao.equals("SIM"));
+
+        System.out.println("Sistema encerrado.");
+        sc.close();
     }
 }
