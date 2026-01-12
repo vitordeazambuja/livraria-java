5930cf495724045df6137b2b45cbbc4f339fceff
Vitor <vitordeazambuja@gmail.com>
2026-01-12 19:25:32 -0300

    fix: aplicado encapsulamento e incrementacao automatica de id

--- src/Autor.java
+++ src/Autor.java
@@ -1,13 +1,37 @@
 import java.time.LocalDate;
 
 public class Autor {
-    int id;
-    String nome;
-    LocalDate dataNascimento;
+    private static int nextId = 0;
 
-    public Autor(int id, String nome, LocalDate dataNascimento) {
-        this.id = id;
+    private final int id;
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Autor(String nome, LocalDate dataNascimento) {
+        this.id = nextId++;
         this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    //getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    //setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
 }
