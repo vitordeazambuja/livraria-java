1abc05e77158e7c3bfc3079eb31cd86a1e703a43
Vitor <vitordeazambuja@edu.uniube.br>
2026-01-12 08:55:45 -0300

    fix: aplicado encapsulamento

--- src/Livro.java
+++ src/Livro.java
@@ -1,12 +1,12 @@
 import java.time.LocalDate;
 
 public class Livro {
-    int id;
-    String titulo;
-    Autor autor;
-    boolean disponivel;
-    LocalDate dataCadastro;
-    LocalDate dataAtualizacao;
+    private int id;
+    private String titulo;
+    private Autor autor;
+    private boolean disponivel;
+    private LocalDate dataCadastro;
+    private LocalDate dataAtualizacao;
 
     public Livro(int id, String titulo, Autor autor, boolean disponivel) {
         this.id = id;
@@ -14,4 +14,27 @@ public class Livro {
         this.autor = autor;
         this.disponivel = disponivel;
     }
+
+    // getters
+    public int getId() {
+        return id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public Autor getAutor() {
+        return autor;
+    }
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+    public LocalDate getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    //setters
+
 }
