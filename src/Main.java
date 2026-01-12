19d0877ce55927c683eb34046d61b699b2407d3f
Vitor <vitordeazambuja@gmail.com>
2026-01-11 21:07:17 -0300

    fix: ajustado menu para mostrar multiplas opcoes e chamar um switch case

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String opcao;
+        int opcao;
 
         do{
             System.out.println("Sistema de Livraria");
-            System.out.println("Deseja ver a lista de livros disponíveis?");
-            opcao = sc.nextLine().trim().toUpperCase();
+            System.out.println("1 - Listar livros disponíveis");
+            System.out.println("2 - Registrar empréstimo");
+            System.out.println("3 - Registrar devolução");
+            System.out.println("0 - Sair");
+            System.out.println("");
+            System.out.print("Opção: ");
+            opcao = Integer.parseInt(sc.nextLine());
 
-        } while(opcao.equals("SIM"));
+            switch(opcao){
+                case 1:
+                    break;
+            }
+
+        } while(opcao != 0);
 
         System.out.println("Sistema encerrado.");
         sc.close();
